af27f5e6d0a6cacb869186bb674da76ceeef640a
Soyeong Park <mb535622@gmail.com>
2026-04-10 19:55:29 +0900

    chore: 모노레포 패키지 세팅 (#5)
    * chore: packages/core 생성
    
    * chore: 루트 package.json 스크립트 추가
    
    * chore: apps/web에 @repo/core 연결
    
    * fix: 리뷰 반영 (react peerDependencies 추가)
    
    * fix: 리뷰 반영 (named export 적용)
    
    * fix: 리뷰 반영 (불필요한 @repo/ui 의존성 제거)
    
    * fix: 타입 에러 수정
    
    * fix: 미사용 import 제거

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,4 @@
-import { Button } from '@repo/ui/button';
 import Image, { type ImageProps } from 'next/image';
-
 import styles from './page.module.css';
 
 type Props = Omit<ImageProps, 'src'> & {
@@ -45,9 +43,6 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
       </main>
       <footer className={styles.footer}>
         <a
---
+++ packages/core/src/index.ts
@@ -0,0 +1,3 @@
+export { formatDate } from './utils/example';
+export type { UserT } from './types/example';
+export { useCounter } from './hooks/example';
