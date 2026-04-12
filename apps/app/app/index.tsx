c3ca17a0b424adc269ed9b8ced8fb9759da612b3
Jung Sun A <amber0809@naver.com>
2026-04-12 13:33:35 +0900

    chore: App 서비스 기초 설정 (#10)
    * chore: docs 앱 삭제
    
    * chore: RN+expo 프로젝트 생성
    
    * chore: app 관련 스크립트 추가
    
    * chore: app tsconfig 경로 오류 수정
    
    * chore: app 불필요한 파일 삭제
    
    * feat: webview 설정
    
    * feat: Webview 디버깅 dev 환경에서만 가능하도록 변경
    
    * chore: update package.json

---
+++ apps/app/app/index.tsx
@@ -0,0 +1,26 @@
+import { KeyboardAvoidingView, Platform } from 'react-native'
+import Webview from 'react-native-webview'
+
+export default function Page() {
+  return (
+    // REF: https://github.com/react-native-webview/react-native-webview/blob/5bc526fce5b9d6225df183bdf3d8cf542007d90a/docs/Reference.md
+    <KeyboardAvoidingView
+      style={{ flex: 1, backgroundColor: 'white' }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <Webview
+        style={{ flex: 1 }}
+        /**
+         * - ios simulator 사용 시: `http://localhost:3000`
+         * - 실기기 사용 시: LAN IP 주소 ex) `http://192.0.0.1:3000`
+         */
+        // TEMP: URI env에 등록하여 사용 예정
+        source={{ uri: 'https://www.naver.com/' }}
+        allowsBackForwardNavigationGestures
+        cacheEnabled
+        webviewDebuggingEnabled={__DEV__}
+        startInLoadingState
+      />
+    </KeyboardAvoidingView>
+  )
+}
