232a313b468acb1ff8a494bf0116f55a8ab85a68
Jung Sun A <amber0809@naver.com>
2026-05-13 10:07:20 +0900

    chore: useWebBridgeMessage 에러 로그 log > error로 변경

--- apps/app/hooks/useWebBridgeMessage.ts
+++ apps/app/hooks/useWebBridgeMessage.ts
@@ -8,16 +8,21 @@ export const useWebBridgeMessage = (handler: (message: WebBridgeMessageT) => voi
     (event: WebViewMessageEvent) => {
       try {
         const message = event.nativeEvent.data;
-        if (!message) return console.log('[WEBVIEW] 수신 메시지 존재하지 않음');
+        if (!message) {
+          console.error('[WEBVIEW] 수신 메시지 존재하지 않음');
+          return;
+        }
 
         const parsedMessage = JSON.parse(message);
-        if (!isWebBridgeMessageT(parsedMessage))
-          return console.log('[WEBVIEW] 수신 메시지 형식 오류', parsedMessage);
+        if (!isWebBridgeMessageT(parsedMessage)) {
+          console.error('[WEBVIEW] 수신 메시지 형식 오류', parsedMessage);
+          return;
+        }
 
-        console.log('[WEBVIEW] 수신 메시지:', parsedMessage);
+        console.error('[WEBVIEW] 수신 메시지:', parsedMessage);
         handler(parsedMessage);
       } catch {
-        console.log('[WEBVIEW] 오류', event.nativeEvent.data);
+        console.error('[WEBVIEW] 오류', event.nativeEvent.data);
       }
     },
     [handler]
