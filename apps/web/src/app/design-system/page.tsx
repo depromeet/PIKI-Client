b243f147dd2c3010600d735b7cdf332a5e6ba3cf
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2026-05-14 23:44:00 +0900

    temp: 디자인 시스템 확인 페이지 추가

---
+++ apps/web/src/app/design-system/page.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+export default function DesignSystemPage() {
+  return (
+    <div className="flex h-dvh w-full flex-col items-center justify-center gap-4 overflow-y-auto p-4">
+      <h1 className="mb-10 text-2xl font-bold">Design System</h1>
+      <LinkItem href="/design-system/color">Color</LinkItem>
+      <LinkItem href="/design-system/typography">Typography</LinkItem>
+      <LinkItem href="/design-system/image">Image / Skeleton UI</LinkItem>
+      <LinkItem href="/design-system/button">Button</LinkItem>
+      <LinkItem href="/design-system/input">Input</LinkItem>
+      <LinkItem href="/design-system/icon">Icons</LinkItem>
+    </div>
+  );
+}
+
+const LinkItem = ({ href, children }: { href: string; children: React.ReactNode }) => {
+  return (
+    <Link href={href} className="w-full rounded-md bg-black py-4 text-center text-white">
+      {children}
+    </Link>
+  );
+};
