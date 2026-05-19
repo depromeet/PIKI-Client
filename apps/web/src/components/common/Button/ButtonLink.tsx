318c71c63ed2444eb835f119404aef914c03c941
joyeongchan <tigerbone@naver.com>
2026-05-20 00:05:57 +0900

    feat: Button 디자인을 적용한 ButtonLink 컴포넌트 추가

---
+++ apps/web/src/components/common/Button/ButtonLink.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import type { ComponentProps, ReactNode } from 'react';
+
+import { cn } from '@/utils/cn';
+
+import { type ButtonStyleProps, buttonStyles } from './Button.style';
+
+type ButtonLinkProps = Omit<ComponentProps<typeof Link>, 'children'> &
+  ButtonStyleProps & {
+    leadingIcon?: ReactNode;
+    children?: ReactNode;
+  };
+
+function ButtonLink({
+  variant,
+  size,
+  icon = 'none',
+  leadingIcon,
+  className,
+  children,
+  ...rest
+}: ButtonLinkProps) {
+  return (
+    <Link className={cn(buttonStyles({ variant, size, icon }), className)} {...rest}>
+      {icon === 'leading' && leadingIcon}
+      {children}
+    </Link>
+  );
+}
+
+export default ButtonLink;
