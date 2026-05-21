834316be757fb624a4d5c007e4378ee876e3a385
kanghaeun <xgkg0330@jnu.ac.kr>
2026-05-21 23:59:59 +0900

    feat: 토너먼트 생성 페이지 섹션 연결

---
+++ apps/web/src/app/tournament/create/page.tsx
@@ -0,0 +1,15 @@
+import InviteFriends from './components/inviteFriends/InviteFriends';
+import TournamentHeader from './components/tournamentHeader/TournamentHeader';
+import WishBasket from './components/wishBasket/WishBasket';
+
+function TournamentCreatePage() {
+  return (
+    <>
+      <TournamentHeader />
+      <InviteFriends />
+      <WishBasket />
+    </>
+  );
+}
+
+export default TournamentCreatePage;
