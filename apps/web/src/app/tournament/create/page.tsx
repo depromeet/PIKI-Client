e46cfe1936449b09e7455d7b9442174c89b96ab4
kanghaeun <xgkg0330@jnu.ac.kr>
2026-05-22 00:04:17 +0900

    style: 토너먼트 준비 페이지 화면 높이 대응

--- apps/web/src/app/tournament/create/page.tsx
+++ apps/web/src/app/tournament/create/page.tsx
@@ -6,14 +6,18 @@ import WishBasket from './components/wishBasket/WishBasket';
 
 function TournamentCreatePage() {
   return (
-    <>
-      <TournamentHeader />
-      <InviteFriends />
-      <WishBasket />
-      <Button variant="primary" size="lg">
-        토너먼트 시작하기
-      </Button>
-    </>
+    <div className="flex min-h-screen flex-col px-5">
+      <div className="flex flex-1 flex-col gap-4 pt-[80px]">
+        <TournamentHeader />
+        <InviteFriends />
+        <WishBasket />
+      </div>
+      <div className="pb-8">
+        <Button variant="primary" size="lg">
+          토너먼트 시작하기
+        </Button>
+      </div>
+    </div>
   );
 }
 
