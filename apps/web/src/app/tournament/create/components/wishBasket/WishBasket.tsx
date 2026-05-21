64c162878f173c77721fd3a353f2edb236227821
kanghaeun <xgkg0330@jnu.ac.kr>
2026-05-22 03:12:00 +0900

    feat: 상품 등록하기 버튼 추가

--- apps/web/src/app/tournament/create/components/wishBasket/WishBasket.tsx
+++ apps/web/src/app/tournament/create/components/wishBasket/WishBasket.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 
 import { EMPTY_WISH_ITEMS } from '@/app/tournament/create/consts/wishBasketConsts';
+import AddIcon from '@/assets/icons/fill/add.svg';
 import basketImg from '@/assets/images/basket-gray.png';
+import Button from '@/components/common/Button/Button';
 
 import WishItem from './WishItem';
 
@@ -10,10 +12,16 @@ function WishBasket() {
     <div className="relative mx-auto aspect-[356/464] w-full">
       <Image src={basketImg} alt="장바구니" fill sizes="100vw" className="object-contain" />
       <div className="absolute inset-0 flex items-center justify-center">
-        <div className="grid w-[45%] grid-cols-2 gap-4">
+        <div className="relative grid w-[45%] grid-cols-2 gap-4">
           {EMPTY_WISH_ITEMS.map((emptyWishItem, index) => (
             <WishItem key={index} index={index} url={emptyWishItem.imageUrl} />
           ))}
+          <Button
+            icon="only"
+            className="absolute top-1/2 left-1/2 size-[60px] -translate-x-1/2 -translate-y-1/2 shadow-lg"
+          >
+            <AddIcon width={32} height={32} className="text-white" aria-hidden />
+          </Button>
         </div>
       </div>
     </div>
