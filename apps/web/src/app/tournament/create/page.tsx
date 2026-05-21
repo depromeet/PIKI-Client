7f58932eeac4adfec2beeda232e50515c97f38af
kanghaeun <xgkg0330@jnu.ac.kr>
2026-05-22 00:03:26 +0900

    feat: 토너먼트 시작하기 버튼 추가

--- apps/web/src/app/tournament/create/page.tsx
+++ apps/web/src/app/tournament/create/page.tsx
@@ -1,3 +1,5 @@
+import Button from '@/components/common/Button/Button';
+
 import InviteFriends from './components/inviteFriends/InviteFriends';
 import TournamentHeader from './components/tournamentHeader/TournamentHeader';
 import WishBasket from './components/wishBasket/WishBasket';
@@ -8,6 +10,9 @@ function TournamentCreatePage() {
       <TournamentHeader />
       <InviteFriends />
       <WishBasket />
+      <Button variant="primary" size="lg">
+        토너먼트 시작하기
+      </Button>
     </>
   );
 }
