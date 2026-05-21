6f59a9b819664543539cfa95c648f046e2db0cf3
kanghaeun <xgkg0330@jnu.ac.kr>
2026-05-21 23:13:59 +0900

    feat: 토너먼트 준비 헤더 섹션 구현

---
+++ apps/web/src/app/tournament/create/components/tournamentHeader/TournamentHeader.tsx
@@ -0,0 +1,10 @@
+function TournamentHeader() {
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <p className="title-1">봄 아우터 고르기</p>
+      <p className="subtitle-1 text-gray-300">최소 2개부터 토너먼트를 시작할 수 있어요.</p>
+    </div>
+  );
+}
+
+export default TournamentHeader;
