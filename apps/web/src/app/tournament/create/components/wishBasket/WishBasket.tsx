ff35c394ce2d1a13049e17d779c3ed2d963c1c1a
kanghaeun <xgkg0330@jnu.ac.kr>
2026-05-22 02:56:45 +0900

    feat: ProductImage 기반 WishItem 컴포넌트 추가

--- apps/web/src/app/tournament/create/components/wishBasket/WishBasket.tsx
+++ apps/web/src/app/tournament/create/components/wishBasket/WishBasket.tsx
@@ -1,11 +1,21 @@
 import Image from 'next/image';
 
+import { EMPTY_WISH_ITEMS } from '@/app/tournament/create/consts/wishBasketConsts';
 import basketImg from '@/assets/images/basket-gray.png';
+
+import WishItem from './WishItem';
 
 function WishBasket() {
   return (
-    <div className="flex flex-col items-center">
-      <Image src={basketImg} alt="장바구니" width={356} height={464} className="h-auto w-full" />
+    <div className="relative mx-auto aspect-[356/464] w-full">
+      <Image src={basketImg} alt="장바구니" fill sizes="100vw" className="object-contain" />
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="grid w-[45%] grid-cols-2 gap-4">
+          {EMPTY_WISH_ITEMS.map((emptyWishItem, index) => (
+            <WishItem key={index} index={index} url={emptyWishItem.imageUrl} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
