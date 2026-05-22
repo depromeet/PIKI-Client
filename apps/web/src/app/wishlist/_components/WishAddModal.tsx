c9778247ccfd112a1caca6d572eff7aca129edcf
soyeong <mb535622@sookmyung.ac.kr>
2026-05-23 00:34:50 +0900

    fix: 코드 리뷰 반영

--- apps/web/src/app/wishlist/_components/WishAddModal.tsx
+++ apps/web/src/app/wishlist/_components/WishAddModal.tsx
@@ -21,9 +21,9 @@ function WishAddModal({ isOpen, onClose, title, options }: WishAddModalProps) {
 
   return createPortal(
     <>
-      <div className="fixed inset-0 z-40 bg-black/40" onClick={onClose} />
-      <div className="fixed left-1/2 top-1/2 z-50 flex w-[362px] -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-[15px] rounded-[24px] bg-white p-5">
-        <p className="heading-1 text-[#2D3037]">{title}</p>
+      <div className="fixed inset-0 z-40 bg-black/40" aria-hidden="true" onClick={onClose} />
+      <div role="dialog" aria-modal="true" aria-labelledby="modal-title" className="fixed left-1/2 top-1/2 z-50 flex w-[362px] -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-[15px] rounded-[24px] bg-white p-5">
+        <p id="modal-title" className="heading-1 text-[#2D3037]">{title}</p>
 
         <div className="flex w-full flex-col gap-2">
           {options.map(({ icon, label, description, onClick }) => (
