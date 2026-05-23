f7479ef796b98febb17b4db39d203d364fc850b3
kanghaeun <xgkg0330@jnu.ac.kr>
2026-05-23 15:54:32 +0900

    refactor: 토너먼트 버튼 로직 분리

--- apps/web/src/app/tournament/create/page.tsx
+++ apps/web/src/app/tournament/create/page.tsx
@@ -1,8 +1,8 @@
-import Button from '@/components/common/Button/Button';
-
 import InviteFriends from './components/inviteFriends/InviteFriends';
 import TournamentHeader from './components/tournamentHeader/TournamentHeader';
+import TournamentStartButton from './components/tournamentStartButton/TournamentStartButton';
 import WishBasketCarousel from './components/wishBasket/WishBasketCarousel';
+import WishBasketStatus from './components/wishBasketStatus/WishBasketStatus';
 
 function TournamentCreatePage() {
   return (
@@ -10,12 +10,11 @@ function TournamentCreatePage() {
       <div className="flex flex-1 flex-col gap-4 pt-[80px]">
         <TournamentHeader />
         <InviteFriends />
+        <WishBasketStatus />
         <WishBasketCarousel />
       </div>
       <div className="pb-8">
-        <Button variant="primary" size="lg">
-          토너먼트 시작하기
-        </Button>
+        <TournamentStartButton count={0} />
       </div>
     </div>
   );
