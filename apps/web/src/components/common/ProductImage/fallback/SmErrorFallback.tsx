346892509c35fd32cdadaa8df1470c65bf618e20
kanghaeun <xgkg0330@jnu.ac.kr>
2026-05-23 16:20:41 +0900

    refactor: LgErrorFallback, LoadingFallback, SmErrorFallback 컴포넌트의 export 방식 수정

--- apps/web/src/components/common/ProductImage/fallback/SmErrorFallback.tsx
+++ apps/web/src/components/common/ProductImage/fallback/SmErrorFallback.tsx
@@ -1,9 +1,11 @@
 import WarningIconFill from '@/assets/icons/fill/warning.svg';
 
-export default function SmErrorFallback() {
+function SmErrorFallback() {
   return (
     <div className="absolute inset-0 flex items-center justify-center">
       <WarningIconFill width={30} height={30} className="text-red-400" aria-hidden />
     </div>
   );
 }
+
+export default SmErrorFallback;
