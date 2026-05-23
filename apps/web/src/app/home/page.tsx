509de54b699fd9c6c96a5ec305a641c0a99e1311
joyeongchan <tigerbone@naver.com>
2026-05-24 00:11:15 +0900

    style: icon secondary로 변경

--- apps/web/src/app/home/page.tsx
+++ apps/web/src/app/home/page.tsx
@@ -51,7 +51,7 @@ function HomePage() {
               type="button"
               className="flex h-[54px] w-full items-center justify-center gap-2 rounded-[12px] bg-bg-layer-default px-9"
             >
-              <LoginIconOutline className="size-6 text-text-neutral-primary" />
+              <LoginIconOutline className="size-6 text-icon-neutral-secondary" />
               <span className="body-1-semibold text-text-neutral-primary">초대 토너먼트 입장</span>
             </button>
           </div>
