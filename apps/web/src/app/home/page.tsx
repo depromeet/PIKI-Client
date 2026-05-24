aef9f4793c4923de01e1033f004a67c90eda7d5b
조영찬 <tigerbone@naver.com>
2026-05-24 17:21:02 +0900

    feat: 홈 페이지 및 토너먼트 생성 진입 UI 구현 (#86)
    * feat: 홈 페이지 레이아웃 초안 추가
    
    * feat: 홈 페이지 로고 및 아이콘 적용
    
    * feat: StateChip 컴포넌트 추가 및 홈 페이지 프로필 적용
    
    * feat: UserProfileGroup 컴포넌트 추가
    
    * feat: TournamentCard 컴포넌트 추가
    
    * fix: 헤더 및 토너먼트 카드 아이콘을 시안에 맞춤
    
    * feat: 홈 페이지에 토너먼트 생성 Dialog 연결
    
    * style: icon secondary로 변경
    
    * refactor: 홈 페이지를 루트 경로로 이동
    
    * chore: 하단 임시 네비게이션 제거
    
    * feat: 위시템 담기 Dialog 추가
    
    * feat: 위시템 담기 Dialog 추가 및 위시리스트 페이지 연결
    
    * test: commit

--- apps/web/src/app/home/page.tsx
+++
@@ -1,5 +0,0 @@
-function HomePage() {
-  return <div>main</div>;
-}
-
-export default HomePage;
---
+++ apps/web/src/mocks/users.ts
@@ -0,0 +1,9 @@
+import type { UserT } from '@/components/common/UserProfileGroup/userProfileConstants';
+
+export const MOCK_USERS: UserT[] = [
+  { id: 1, profileType: 'blue', name: '사용자 1' },
+  { id: 2, profileType: 'yellow', name: '사용자 2' },
+  { id: 3, profileType: 'blue', name: '사용자 3' },
+  { id: 4, profileType: 'yellow', name: '사용자 4' },
+  { id: 5, profileType: 'blue', name: '사용자 5' },
+];
