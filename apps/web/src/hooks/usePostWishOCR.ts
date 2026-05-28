8e2657a6c286be569cb7eb91b019c449170faa10
Jung Sun A <amber0809@naver.com>
2026-05-28 09:00:14 +0900

    feat: 이미지로 위시 담기 시 invalidate 처리

--- apps/web/src/hooks/usePostWishOCR.ts
+++ apps/web/src/hooks/usePostWishOCR.ts
@@ -1,10 +1,15 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { postWishOCR } from '@/apis/postWishOCR';
 
 export const usePostWishOCR = () => {
+  const queryClient = useQueryClient();
+
   const { mutate: postWishOCRMutation } = useMutation({
     mutationFn: (formData: FormData) => postWishOCR(formData),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['wishlists'] });
+    },
   });
 
   return { postWishOCRMutation };
