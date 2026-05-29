f2dac720ef32b8a4b4972496d6e7ca5bbbbd94b4
kanghaeun <xgkg0330@jnu.ac.kr>
2026-05-29 19:33:30 +0900

    feat: 아이템 READY/FAILED 상태 시 토너먼트 데이터 재조회

--- apps/web/src/app/tournament/[id]/create/_hooks/useGetTournamentItem.ts
+++ apps/web/src/app/tournament/[id]/create/_hooks/useGetTournamentItem.ts
@@ -1,16 +1,21 @@
-import { useQuery } from '@tanstack/react-query';
 import type { Query } from '@tanstack/react-query';
-
-import type { GetTournamentItemResponseT } from '../_types/tournament';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { getTournamentItem } from '../_apis/getTournamentItem';
+import type { GetTournamentItemResponseT } from '../_types/tournament';
 
 export const useGetTournamentItem = (tournamentId: number, tournamentItemId: number | null) => {
+  const queryClient = useQueryClient();
+
   const { data: tournamentItemData } = useQuery({
     queryKey: ['tournamentItem', tournamentId, tournamentItemId],
     queryFn: () => getTournamentItem(tournamentId, tournamentItemId!),
     enabled: tournamentItemId !== null,
     refetchInterval: (query: Query<GetTournamentItemResponseT>) => {
+      if (query.state.data?.status === 'READY' || query.state.data?.status === 'FAILED') {
+        queryClient.invalidateQueries({ queryKey: ['tournament', tournamentId] });
+        return false;
+      }
       if (query.state.data?.status === 'PROCESSING') return 3_000;
       return false;
     },
