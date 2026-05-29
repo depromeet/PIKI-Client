6bb9bf6aa6d1e2186106a518f8d969c9cf13bcd7
soyeong <mb535622@sookmyung.ac.kr>
2026-05-30 00:02:28 +0900

    feat: 위시탭·토너먼트탭 empty state 추가

--- apps/web/src/app/wishlist/_components/TournamentHistoryContent.tsx
+++ apps/web/src/app/wishlist/_components/TournamentHistoryContent.tsx
@@ -1,32 +1,16 @@
-'use client';
-
-import { useState } from 'react';
-
+import TrophyIconFill from '@/assets/icons/fill/trophy.svg';
 import BottomTabBar from '@/components/common/bottom-tab-bar';
 
-import WishAddDialog from './WishAddDialog';
-import WishlistFabArea from './WishlistFabArea';
-
 function TournamentHistoryContent() {
-  const [isAddDialogOpen, setIsAddDialogOpen] = useState(false);
-
   return (
     <>
-      <main className="flex flex-1 items-center justify-center pb-24">
-        <p className="body-1-medium text-gray-300">토너먼트 기록이 없어요</p>
+      <main className="flex flex-1 flex-col items-center justify-center gap-3 pb-24">
+        <TrophyIconFill width={32} height={32} className="text-gray-200" />
+        <p className="body-1-semibold text-text-neutral-tertiary">아직 진행한 토너먼트가 없어요</p>
       </main>
       <div className="fixed bottom-10 left-1/2 z-20 flex -translate-x-1/2 items-center gap-3">
         <BottomTabBar />
       </div>
-      <WishlistFabArea
-        isDeleteMode={false}
-        isDeleteDisabled={true}
-        onAddItem={() => setIsAddDialogOpen(true)}
-        // TODO: 토너먼트 기록 삭제 API 생기면 구현
-        onEnterDeleteMode={() => {}}
-        onConfirmDelete={() => {}}
-      />
-      <WishAddDialog open={isAddDialogOpen} onOpenChange={setIsAddDialogOpen} />
     </>
   );
 }
