2c062bc5226c55cb2b66622e25074b8d0e8dcd2f
kanghaeun <xgkg0330@jnu.ac.kr>
2026-05-30 21:56:06 +0900

    refactor: TournamentHeader 컴포넌트에서 p 태그를 h1으로 변경

--- apps/web/src/app/tournament/[id]/create/_components/tournamentHeader/TournamentHeader.tsx
+++ apps/web/src/app/tournament/[id]/create/_components/tournamentHeader/TournamentHeader.tsx
@@ -5,7 +5,7 @@ type TournamentHeaderProps = {
 function TournamentHeader({ name }: TournamentHeaderProps) {
   return (
     <div className="flex flex-col items-center">
-      <p className="title-1">{name}</p>
+      <h1 className="title-1">{name}</h1>
     </div>
   );
 }
