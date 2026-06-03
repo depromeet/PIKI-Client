070172d472f2477aaff63b31485322998bfba695
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2026-06-03 12:36:24 +0900

    refactor: isWebview 함수 서버/클라이언트 환경에서 모두 사용할 수 있도록 변경

--- apps/web/src/utils/webBridge.ts
+++ apps/web/src/utils/webBridge.ts
@@ -1,4 +1,4 @@
-import type { WebBridgeMessageT } from '@piki/core';
+import { WEBVIEW_UA_TOKEN, type WebBridgeMessageT } from '@piki/core';
 
 type RNWebViewWindowT = {
   ReactNativeWebView: {
@@ -17,10 +17,30 @@ const isRNWebViewWindow = (window: unknown): window is RNWebViewWindowT => {
   return typeof rnWebview['postMessage'] === 'function';
 };
 
-/** 현재 환경이 RN WebView인지 여부 */
-export const isWebview = () => {
-  if (typeof window === 'undefined') return false;
-  return isRNWebViewWindow(window);
+/**
+ * @param userAgentFromServer 서버 환경일 때 전달받은 user-agent
+ * @returns 현재 환경이 RN WebView(앱)인 경우 true, 그 외 경우 false
+ *
+ * @example 클라이언트 환경일 때
+ * isWebview()
+ *
+ * @example 서버 환경일 때
+ * const headerStore = await headers();
+ * const userAgent = headerStore.get('user-agent');
+ * isWebview(userAgent)
+ */
+export const isWebview = (userAgentFromServer: string | null = null) => {
+  /** 클라이언트 환경일 떄 */
+  if (typeof window !== 'undefined') {
+    const isAppByUA = navigator.userAgent.includes(WEBVIEW_UA_TOKEN);
+    const isAppByWindow = isRNWebViewWindow(window);
+
+    return isAppByUA || isAppByWindow;
+  }
+
+  /** 서버 환경일 때 */
+  if (userAgentFromServer) return userAgentFromServer.includes(WEBVIEW_UA_TOKEN);
+  return false;
 };
 
 /**
