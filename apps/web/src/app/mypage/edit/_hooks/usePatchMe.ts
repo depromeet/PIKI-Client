7bd13fb580e97e8aee49f604c0566b6b6448df87
Jung Sun A <amber0809@naver.com>
2026-06-08 17:27:42 +0900

    feat: 유저 데이터 수정 후 success, error 처리 보완

--- apps/web/src/app/mypage/edit/_hooks/usePatchMe.ts
+++ apps/web/src/app/mypage/edit/_hooks/usePatchMe.ts
@@ -1,8 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { isAxiosError } from 'axios';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
+
+import { ROUTES } from '@/consts/route';
+import type { ApiErrorResponseT } from '@/types/api';
 
 import { patchMe } from '../_apis/patchMe';
 
 export const usePatchMe = () => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
   const { mutate: patchMeMutation, isPending: isPatchMePending } = useMutation({
     mutationFn: ({ image, nickname }: { image?: File; nickname?: string }) => {
       const formData = new FormData();
@@ -10,6 +19,15 @@ export const usePatchMe = () => {
       if (nickname) formData.append('nickname', nickname);
 
       return patchMe(formData);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['me'] });
+      router.replace(ROUTES.MYPAGE);
+    },
+    onError: error => {
+      if (!isAxiosError<ApiErrorResponseT>(error) || !error.response) return;
+
+      toast.error(error.response.data.detail);
     },
   });
 
