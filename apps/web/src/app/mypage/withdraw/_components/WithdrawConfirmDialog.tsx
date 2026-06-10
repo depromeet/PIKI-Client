695f905c634e0d6db2f3252d52a1ec154e6d7ac8
Jung Sun A <amber0809@naver.com>
2026-06-10 13:25:23 +0900

    chore: 유저 탈퇴 중복 에러 처리 삭제

--- apps/web/src/app/mypage/withdraw/_components/WithdrawConfirmDialog.tsx
+++ apps/web/src/app/mypage/withdraw/_components/WithdrawConfirmDialog.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { isAxiosError } from 'axios';
 import { useState } from 'react';
-import { toast } from 'sonner';
 
 import { WarningIconFill } from '@/assets/icons';
 import BottomCta from '@/components/bottom-cta';
@@ -17,7 +15,6 @@ import {
   DialogTrigger,
 } from '@/components/dialog';
 import Spinner from '@/components/spinner';
-import type { ApiErrorResponseT } from '@/types/api';
 
 import { useDeleteMe } from '../_hooks/useDeleteMe';
 
@@ -30,12 +27,7 @@ function WithdrawConfirmDialog() {
     if (isDeleteMePending) return;
 
     deleteMeMutation(void 0, {
-      onError: error => {
-        if (!isAxiosError<ApiErrorResponseT>(error) || !error.response) return;
-
-        setIsOpen(false);
-        toast.error('알 수 없는 오류가 발생했어요.');
-      },
+      onError: () => setIsOpen(false),
     });
   };
 
