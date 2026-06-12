5f7cb746eda3913e2b559b1b225774c48a176c97
Jung Sun A <amber0809@naver.com>
2026-06-12 16:45:39 +0900

    chore: /archive 401 처리 로직 삭제

--- apps/web/src/app/archive/layout.tsx
+++ apps/web/src/app/archive/layout.tsx
@@ -1,9 +1,7 @@
-import { isAxiosError } from 'axios';
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 import { getMe } from '@/apis/getMe';
-import type { ApiErrorResponseT } from '@/types/api';
 import { getLoginPath } from '@/utils/loginRedirect';
 
 type ArchiveLayoutProps = {
@@ -15,16 +13,8 @@ async function ArchiveLayout({ children }: ArchiveLayoutProps) {
   const redirectPath = headerStore.get('x-redirect-path');
 
   /** MEMBER 권한 조회 - 멤버 권한 없으면 로그인 페이지로 리다이렉트 */
-  try {
-    const user = await getMe();
-
-    if (user.identityType !== 'MEMBER') redirect(getLoginPath(redirectPath));
-  } catch (error) {
-    if (isAxiosError<ApiErrorResponseT>(error) && error.response?.status === 401)
-      redirect(getLoginPath(redirectPath));
-
-    throw error;
-  }
+  const user = await getMe();
+  if (user.identityType !== 'MEMBER') redirect(getLoginPath(redirectPath));
 
   return children;
 }
