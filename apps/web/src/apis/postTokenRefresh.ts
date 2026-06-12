93bdcf10fa8cf1154b78ed5b5acfc83b4cb1060d
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2026-06-13 00:23:11 +0900

    fix: token refresh content-type false로 설정하여 415 에러 대응

--- apps/web/src/apis/postTokenRefresh.ts
+++ apps/web/src/apis/postTokenRefresh.ts
@@ -3,10 +3,16 @@ import type { ApiResponseT } from '@/types/api';
 
 import { serverApi } from './server';
 
-export const postTokenRefreshServer = async () => {
+export const postTokenRefreshServer = async (cookies: string) => {
   const response = await serverApi.post<
-    ApiResponseT<{ accessToken: string | null; refreshToken: string | null }>
-  >(ENDPOINTS.AUTH_TOKEN_REFRESH);
+    ApiResponseT<{ access_token: string; refresh_token: string }>
+  >(ENDPOINTS.AUTH_TOKEN_REFRESH, void 0, {
+    headers: {
+      Cookie: cookies,
+      /** Refresh는 Content-Type 헤더 있으면 415 에러 발생 */
+      'Content-Type': false,
+    },
+  });
 
   return response;
 };
