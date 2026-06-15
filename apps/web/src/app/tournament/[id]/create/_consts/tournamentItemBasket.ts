2df8c9c80c922275fd71a3d9675bf933312f6cf0
kanghaeun <xgkg0330@jnu.ac.kr>
2026-06-15 18:48:08 +0900

    chore: EMPTY_BASKET_IMAGES 상수 제거

--- apps/web/src/app/tournament/[id]/create/_consts/tournamentItemBasket.ts
+++ apps/web/src/app/tournament/[id]/create/_consts/tournamentItemBasket.ts
@@ -1,14 +1,5 @@
-import img01 from '../_assets/img01.png';
-import img02 from '../_assets/img02.png';
-import img03 from '../_assets/img03.png';
-import img04 from '../_assets/img04.png';
-import img05 from '../_assets/img05.png';
-import img06 from '../_assets/img06.png';
-
 export const ITEMS_PER_BASKET = 8;
 export const BASKET_COUNT = 4;
 
 /** 매인 캐러셀 너비가 화면에서 차지하는 퍼센트 (인접 바구니는 100% - 이 값만큼 뺀 나머지 너비를 인접 바구니가 차지) */
 export const BASKET_CAROUSEL_SLIDE_SIZE_PERCENT = 90;
-
-export const EMPTY_BASKET_IMAGES = [img01, img02, img03, img04, img05, img06];
