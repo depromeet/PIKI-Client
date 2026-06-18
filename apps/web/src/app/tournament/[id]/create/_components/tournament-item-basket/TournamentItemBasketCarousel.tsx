d8d56034d24a489baceb96b9061e1f822a818def
kanghaeun <145974230+kanghaeun@users.noreply.github.com>
2026-06-18 20:58:44 +0900

    fix: 장바구니 8개 가득 찬 상태에서 아이템 삭제 시 장바구니 사라짐 버그 수정 (#254)

--- apps/web/src/app/tournament/[id]/create/_components/tournament-item-basket/TournamentItemBasketCarousel.tsx
+++ apps/web/src/app/tournament/[id]/create/_components/tournament-item-basket/TournamentItemBasketCarousel.tsx
@@ -58,11 +58,11 @@ function TournamentItemBasketCarousel({
 
   /** 초기 이미지 위치 틀어짐 방지 */
   useLayoutEffect(() => {
-    if (!carouselApi) return;
+    if (!carouselApi || !isCarouselEnabled) return;
 
     carouselApi.reInit();
     carouselApi.scrollTo(carouselApi.selectedScrollSnap(), true);
-  }, [carouselApi, activeBasketCount]);
+  }, [carouselApi, activeBasketCount, isCarouselEnabled]);
 
   useEffect(() => {
     if (!carouselApi) return;
@@ -90,7 +90,9 @@ function TournamentItemBasketCarousel({
   const gap = isCarouselEnabled ? 16 : 0;
   let basketMaxHeight: number | undefined;
   if (containerHeight) {
-    basketMaxHeight = containerHeight - (indicatorHeight ?? 0) - gap;
+    basketMaxHeight = isCarouselEnabled
+      ? containerHeight - (indicatorHeight ?? 0) - gap
+      : containerHeight;
   }
 
   if (!isCarouselEnabled) {
