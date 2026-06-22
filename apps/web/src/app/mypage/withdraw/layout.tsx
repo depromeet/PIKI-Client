634bcc1696a21ee9eacf8b9f7e66b8bfb05d795c
Jung Sun A <amber0809@naver.com>
2026-06-22 16:48:28 +0900

    refactor: layout 권한 확인 시 fetchQuery 활용하여 다중 호출되지 않도록 방지

--- apps/web/src/app/mypage/withdraw/layout.tsx
+++ apps/web/src/app/mypage/withdraw/layout.tsx
@@ -1,6 +1,6 @@
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import { isAxiosError } from 'axios';
-import { notFound, redirect } from 'next/navigation';
+import { headers } from 'next/headers';
 
 import { getMe } from '@/apis/getMe';
 import { ROUTES } from '@/consts/route';
@@ -8,11 +8,16 @@ import type { ApiErrorResponseT } from '@/types/api';
 import { getQueryClient } from '@/utils/queryClient';
 
 async function MyPageMemberOnlyLayout({ children }: { children: React.ReactNode }) {
+  const headerStore = await headers();
+  const redirectPath = headerStore.get('x-redirect-path');
   const queryClient = getQueryClient();
+
   /** 유저 정보 조회 */
   try {
-    const userData = await getMe();
-    queryClient.setQueryData(['me'], userData);
+    const userData = await queryClient.fetchQuery({
+      queryKey: ['me'],
+      queryFn: getMe,
+    });
 
     if (userData.identityType !== 'MEMBER') redirect(ROUTES.LOGIN);
   } catch (error) {
